bd51a021a4e201fadedcefb2762f33c98408a8e8
abunaim34 <farhanadnanfarabi@gmail.com>
2026-06-27 17:26:20 +0600

    Cleaning And Organizing The Auth Middleware And Implementing Update User Profile API

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -62,9 +62,20 @@ const getMyProfile = catchAsync(async (req: Request, res: Response, next: NextFu
 })
 
 
-const updateMyProfile = async() => {
+const updateMyProfile = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id as string
+    const payload = req.body
 
-}
+    const updatedUser = await userService.updateMyProfileInDB(userId, payload)
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: 'User profile update successfully',
+        data: { updatedUser }
+    })
+})
+
 
 export const userController = {
     registerUser, getMyProfile, updateMyProfile
